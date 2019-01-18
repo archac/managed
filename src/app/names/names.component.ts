f334cbe4a26934174163c5c371ea9dd3e880fba3
Arron Chave <chavearron@gmail.com>
2019-01-18 20:24:39 +0000

    Added NGXS and Names Component

---
+++ src/app/names/names.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-names',
+  styleUrls: ['./names.component.css'],
+  templateUrl: `
+    <h1> Names Works! </h1>
+  `,
+})
+export class NamesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
