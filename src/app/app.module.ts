9911a0daadeccf23d68924257f3b096f43d5f49c
Arron Chave <chavearron@gmail.com>
2019-01-19 17:44:27 +0000

    Added text field with debounced search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NamesComponent } from './names/names.component';
 import { NgxsModule } from '@ngxs/store';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { NamesState } from './names/store/names.state';
 import { NamesService } from './names/services/names.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -27,7 +29,9 @@ const appRoutes: Routes = [
     NgxsModule.forRoot([
       NamesState
     ]),
+    NgxsLoggerPluginModule.forRoot(),
     BrowserModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [
--- src/app/names/names.component.ts
+++ src/app/names/names.component.ts
@@ -2,15 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { NamesService } from './services/names.service';
 import { Select } from '@ngxs/store';
 import { NamesState } from './store/names.state';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { Name } from './store/names.model';
 
 @Component({
   selector: 'app-names',
   styleUrls: ['./names.component.css'],
   template: `
-    <h1> Names Works! </h1>
     <button (click)="filter()"> Test api </button>
+    <input type="text" [(ngModel)]="criteria" (keyup)="keyUp.next()">
     <ul>
       <li *ngFor="let name of names$ | async">{{ name.name }}</li>
     </ul>
@@ -20,15 +21,29 @@ export class NamesComponent implements OnInit {
 
   @Select(NamesState.getNames) names$: Observable<Name[]>;
 
+  criteria = '';
+  keyUp = new Subject<string>();
+
   constructor(
     private namesService: NamesService
-  ) { }
+  ) {
+    // Setup the observable that will call the search with a
+    // delay after the user has stopped typing
+    this.keyUp.pipe(
+      debounceTime(500)
+    ).subscribe(() => {
+      this.filter(this.criteria);
+    });
+
+    // do an initial search
+    this.filter(this.criteria);
+  }
 
   ngOnInit() {
   }
 
-  filter() {
-    this.namesService.getNames('ron');
+  filter(criteria: string) {
+    this.namesService.getNames(criteria);
   }
 
 }
